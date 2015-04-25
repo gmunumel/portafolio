32716514405b099b4b02c01285ff1c33ea168e2b
Gabriel Muñumel <gabrielmunumel@gmail.com>
2015-04-25 10:13:07 +0100

    Fixed rails_admin to manage categories and projects

--- db/migrate/20150423230553_create_categories.rb
+++ db/migrate/20150423230553_create_categories.rb
@@ -2,6 +2,7 @@ class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
       t.string :title
+      t.integer :project_id
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150423230553) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
+    t.integer  "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

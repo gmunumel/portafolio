d026f62dc5624f72f6a36105c6f70099c6481247
Gabriel Muñumel <gabrielmunumel@gmail.com>
2015-05-01 19:27:30 +0100

    Fixed problem with long text for li tags in projects

--- db/migrate/20150423131938_create_projects.rb
+++ db/migrate/20150423131938_create_projects.rb
@@ -1,10 +1,10 @@
 class CreateProjects < ActiveRecord::Migration
   def up
     create_table :projects do |t|
-      t.string :title
-      t.text :text
-      t.string :url
-      t.text :key_feature
+      t.string  :title
+      t.text    :text
+      t.string  :url
+      t.text    :key_feature
       t.integer :category_id
 
       t.timestamps null: false

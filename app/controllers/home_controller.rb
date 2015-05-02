8a60e4e2614458c38e269c391631c8809866bd83
Gabriel Muñumel <gabrielmunumel@gmail.com>
2015-05-02 10:10:09 +0100

    Fixed the size of projects to show in index controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @projects = Project.order(:created_at)
+    @projects = Project.order(:created_at).take(2)
     @posts = Post.order(:created_at).take(2)
   end
 end
